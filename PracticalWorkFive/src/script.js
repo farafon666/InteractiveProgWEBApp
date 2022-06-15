09f2aa90c6756b32df6f5a81719ea8fde4a71f71
farafon666 <farafon666@inbox.ru>
2022-06-16 00:21:17 +0400

    add work fivi

--- PracticalWorkFive/src/script.js
+++ PracticalWorkFive/src/script.js
@@ -1,10 +1,10 @@
-import {movie_storage} from './data'
+import {movie_storage} from './data.js'
 
 document.addEventListener('DOMContentLoaded', main)
 
 // Точка входа
 function main() {
-  
+  for (movie of movie_storage) {
+    console.log(movie)
+  }
 }
-
-
